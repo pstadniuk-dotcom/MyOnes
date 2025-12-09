64686c74d796dba1264c4e3cccd7e49a82780df9
Pete Stadniuk <pete@myones.ai>
2025-12-08 19:42:13 -0800

    fix: add cache-control to CORS allowed headers

--- server/index.ts
+++ server/index.ts
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cache-Control, Pragma');
   }
   // SECURITY: Do NOT set any CORS headers for unknown origins
   // The browser will block the request if headers are missing
